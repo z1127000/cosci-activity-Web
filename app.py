60ab53f02b0806ab8919127694031cf64aad1358
z1127000 <73573392+z1127000@users.noreply.github.com>
2024-01-09 10:33:03 +0800

    update simulation maxuser

--- app.py
+++ app.py
@@ -222,6 +222,6 @@ def on_leave(data):
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=80, debug=True)
 
 #ip:140.115.53.202
